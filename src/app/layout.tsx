0f27f9df3d0bd3cc3a4bb731d9f6678a481abada
jeinando <jacobtellezr@gmail.com>
2025-05-12 16:51:24 -0600

    2.0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import Footer from "@/components/Footer";
+
 import MousePointer from "@/components/MouseSpotlight"; 
 
 const geistSans = Geist({
@@ -31,7 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white text-gray-900`}
       >
         <MousePointer /> {/* Aquí lo agregas */}
-        <main>{children}</main> {/* Aquí se renderiza el contenido de las páginas */}
+        <main>{children}</main> 
     
       </body>
     </html>
