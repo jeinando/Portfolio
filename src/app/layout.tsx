0cefd5f9893b8d20dd4455f44bb86f29172cf572
jeinando <jacobtellezr@gmail.com>
2025-05-14 18:34:06 -0600

    Description changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Jacob Tellez ",
-  description: "Personal portfolio",
+  description: "System Engineer",
 };
 
 export default function RootLayout({
